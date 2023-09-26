8a3294f9a87a9608089c05d20204578005589690
sumanadhikari123 <sumanadhikari021@gmail.com>
2023-09-26 13:07:49 +0545

    weather forecasting web app

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",SECRET_KEY: "a6eb194059df758687bee2c9ae615160"
+    },
+};
+
+module.exports= constants;
---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
+
+const weatherData = require('../utils/weatherData');
+
+const port = process.env.PORT || 3000;
+
+const publicStaticDirPath = path.join(__dirname,'../public')
+
+const viewsPath = path.join(__dirname,'../templates/views');
+
+const partialPath = path.join(__dirname,'../templates/partials');
+
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialPath);
+
+
+app.use(express.static(publicStaticDirPath));
+
+app.get('',(req,res) => {
+    res.render('index', {
+        title:'Weather App'
+    });
+});
+
+// localhost:3000/weather?address=london
+app.get('/weather', (req,res) =>{
+    const address = req.query.address;
+    if(!address) {
+        return res.send({
+            error: "You must enter address in search text box"
+        })
+    }
+
+    weatherData(address, (error, {temperature, description, cityName} = {}) => {
+        if(error) {
+            return res.send({
+                error
+            })
+        }
+        console.log(temperature, description, cityName);
+        res.send({
+            temperature,
+            description,
+            cityName
+        })
+    })
+});
+app.get("*", (req,res)=>{
+    res.render('404', {
+        title: "page not found"
+    })
+});
+
+
+app.listen(port, () =>{
+    console.log("server is up and running on port:",port);
+});
+
+
